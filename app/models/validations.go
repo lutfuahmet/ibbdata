b48b58fd92a5611d9e8dd2ffc396ecda150b52ca
ahmetlutfu <l6504167@std.yildiz.edu.tr>
2020-01-19 19:04:59 +0000

    validations completed

--- app/models/validations.go
+++ app/models/validations.go
@@ -1,28 +1,65 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"errors"
+	"strings"
+
+	"github.com/jinzhu/gorm"
+)
 
 // Validate Park
 func (p Park) Validate(db *gorm.DB) {
-
+	if p.ID == 0 || strings.TrimSpace(p.Name) == "" {
+		db.AddError(errors.New("id and name can't be blank"))
+	}
+	if p.ParkLocationID == 0 {
+		db.AddError(errors.New("parklocation can't be blank"))
+	}
+	if p.ParkTypeID == 0 {
+		db.AddError(errors.New("parktype can't be blank"))
+	}
+	if p.ParkZoneID == 0 {
+		db.AddError(errors.New("parkzone can't be blank"))
+	}
+	if p.ParkSubZoneID == 0 {
+		db.AddError(errors.New("parksubzone can't be blank"))
+	}
+	if p.DistrictID == 0 {
+		db.AddError(errors.New("district can't be blank"))
+	}
 }
 
 // Validate ParkLocation
 func (p ParkLocation) Validate(db *gorm.DB) {
-	
+	if p.ID == 0 || strings.TrimSpace(p.Name) == "" {
+		db.AddError(errors.New("id and name can't be blank"))
+	}
 }
 
 // Validate ParkType
 func (p ParkType) Validate(db *gorm.DB) {
-
+	if p.ID == 0 || strings.TrimSpace(p.Name) == "" {
+		db.AddError(errors.New("id and name can't be blank"))
+	}
 }
 
 // Validate ParkZone
 func (p ParkZone) Validate(db *gorm.DB) {
-
+	if p.ID == 0 || strings.TrimSpace(p.Name) == "" {
+		db.AddError(errors.New("id and name can't be blank"))
+	}
 }
 
 // Validate ParkSubZone
 func (p ParkSubZone) Validate(db *gorm.DB) {
+	if p.ID == 0 || strings.TrimSpace(p.Name) == "" || p.ParkZoneID == 0 {
+		db.AddError(errors.New("id, name, parkzone can't be blank"))
+	}
+}
 
+// Validate District
+func (p District) Validate(db *gorm.DB) {
+	if p.ID == 0 || strings.TrimSpace(p.Name) == "" {
+		db.AddError(errors.New("id, name can't be blank"))
+	}
 }
